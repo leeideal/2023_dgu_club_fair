96967914981c26b8a144a3e875074db2028275cd
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-05 02:41:43 +0900

    fix:coflict

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './routes/About';
 import Booth from './routes/Booth';
 import Main from './routes/Main';
+import BoothDetail from './routes/BoothDetail';
+import BoothAdd from './routes/BoothAdd';
 import Introduction from './routes/Introduction';
 
 function Router() {
@@ -11,7 +13,9 @@ function Router() {
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
         <Route path="/booth" element={<Booth />} />
-        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/booth/:str" element={<BoothDetail />} />
+        <Route path="/introduction" element={<Introduction />}/>
+        <Route path="/boothAdd" element={<BoothAdd/>}/>
       </Routes>
     </BrowserRouter>
   );
