443ce1fa6f4a149f557a2d677db714a226bd2fa1
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-04 13:49:12 +0900

    feat : introdcution 페이지 라우팅

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './routes/About';
 import Booth from './routes/Booth';
 import Main from './routes/Main';
+import Introduction from './routes/Introduction';
+
 
 function Router() {
   return (
@@ -10,6 +12,7 @@ function Router() {
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
         <Route path="/booth" element={<Booth />} />
+        <Route path="/introduction" element={<Introduction />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/Introduction.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Introduction = () => {
+  return <>동아리 소개</>;
+};
+
+export default Introduction;
